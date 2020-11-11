f4d1429adbf89acdbe0c7ea52f49b1c7344bbc35
k1te <kmiller343@gmail.com>
2020-11-11 14:45:45 -0600

    removed db variable

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
 const routes = require("./routes/html-routes");
-// const db = require("./models");
+
 
 // Port Set-up
 const PORT = process.env.PORT || 3000;
@@ -22,6 +22,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
 });
 
 app.use("/", routes);
+
 
 app.listen(PORT, function () {
   console.log(`Now listening on port: ${PORT}`);
