19f73c994769907522d4ba74edf02469eb281617
k1te <kmiller343@gmail.com>
2020-11-11 16:06:08 -0600

    rewrote get/post routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,29 +3,42 @@ const db = require("../models/index");
 const apiRouter = express.Router();
 
 apiRouter.get("/api/workouts", (req, res) => {
-  db.Workout.find({}, (error, results) => {
-    if (error) {
-      throw error;
-    } else {
-      res.json(results);
-    }
-  });
-});
+   db.Workout.find()
+     .then(allWorkouts => {
+         res.json(allWorkouts);
+     })
+     .catch(error => {
+         res.json(error);
+     })
+})
 
 apiRouter.post("/api/workouts", (req, res) => {
-  db.Workout.create({}).then((newWorkout) => {
-    res.json(newWorkout);
-  });
-});
+    db.Workout.create()
+      .then(newWorkout => {
+          res.json(newWorkout);
+      })
+      .catch(error => {
+          res.json(error);
+      });
+})
+// apiRouter.post("/api/workouts", (req, res) => {
+//   db.Workout.create(),(error, results) => {
+//     if (error) {
+//         throw error;
+//     } else {
+//         console.log(results);
+//         res.json(results);
+//     }
+//   }
+// });
 
 // apiRouter.put("/api/workouts/:id", (req, res) => {
-//   db.Workout.findOneAndUpdate(
-//     { _id: req.params.id },
-//     { $push: { exercises: req.body } },
-//     (updatedWorkout) => {
+//   const filter = { _id: req.params.id };
+//   const update = req.body;
+  
+//   db.Workout.findOneAndUpdate({ _id: req.params.id }, {$push: {exercises: update}}).then(updatedWorkout => {
 //       res.json(updatedWorkout);
-//     }
-//   );
+//   });
 // });
 
 module.exports = apiRouter;
