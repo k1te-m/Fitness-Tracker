0008a734adfa2ee005d62726f3414d8a8a0caa88
k1te <kmiller343@gmail.com>
2020-11-11 15:17:22 -0600

    Wrote post/get apiRoutes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,3 +1,31 @@
 const express = require("express");
-const router = express.Router();
+const db = require("../models/index");
+const apiRouter = express.Router();
 
+apiRouter.get("/api/workouts", (req, res) => {
+  db.Workout.find({}, (error, results) => {
+    if (error) {
+      throw error;
+    } else {
+      res.json(results);
+    }
+  });
+});
+
+apiRouter.post("/api/workouts", (req, res) => {
+  db.Workout.create({}).then((newWorkout) => {
+    res.json(newWorkout);
+  });
+});
+
+// apiRouter.put("/api/workouts/:id", (req, res) => {
+//   db.Workout.findOneAndUpdate(
+//     { _id: req.params.id },
+//     { $push: { exercises: req.body } },
+//     (updatedWorkout) => {
+//       res.json(updatedWorkout);
+//     }
+//   );
+// });
+
+module.exports = apiRouter;
