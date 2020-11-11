dd309c83a6724706291f2e8b0fb79b1562ee3859
k1te <kmiller343@gmail.com>
2020-11-11 12:02:31 -0600

    wrote basic server

---
+++ server.js
@@ -0,0 +1,27 @@
+// Dependencies
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const db = require("./models");
+
+// Port Set-up
+const PORT = process.env.PORT || 3000;
+
+// Express Set-up
+const app = express();
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
+  useNewUrlParser: true,
+});
+
+const routes = require("./controller/controller");
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log(`Now listening on port: ${PORT}`);
+});
