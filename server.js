9a0543fcfa8346a22cc41e5a8217f95a2ebad8e8
k1te <kmiller343@gmail.com>
2020-11-11 12:31:33 -0600

    server connection established

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-const db = require("./models");
+// const db = require("./models");
 
 // Port Set-up
 const PORT = process.env.PORT || 3000;
@@ -14,13 +14,14 @@ app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+app.use(express.static("/public"));
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/fitness", {
   useNewUrlParser: true,
 });
 
-const routes = require("./controller/controller");
-app.use(routes);
+
+
 
 app.listen(PORT, function() {
     console.log(`Now listening on port: ${PORT}`);
